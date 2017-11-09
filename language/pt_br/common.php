c96e5e7b851cbffe54a1321a83ce7097b1c24f71
phpBBTraducoes <eunaumtenhoid@outlook.com>
2017-11-09 00:19:42 -0200

    Traducao ADD

---
+++ language/pt_br/common.php
@@ -0,0 +1,28 @@
+<?php
+/**
+*
+* @package Topic description
+* @copyright (c) 2016 Rich McGirr (RMcGirr83)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* Brazilian Portuguese translation by eunaumtenhoid (c) 2017 [ver 1.0.2] (https://github.com/phpBBTraducoes)
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+* DO NOT CHANGE
+*/
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'TOPIC_DESC'				=> 'Descrição do tópico',
+));
